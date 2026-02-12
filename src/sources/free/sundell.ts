827e86d90041f64e795bbd57324c6ce3f4f5203b
Lasha Efremidze <efremidzel@hotmail.com>
2026-02-11 23:28:53 -0800

    Remove redundant pattern interfaces

--- src/sources/free/sundell.ts
+++ src/sources/free/sundell.ts
@@ -3,15 +3,12 @@
 import { RssPatternSource, type BasePattern } from './rssPatternSource.js';
 import { createSourceConfig } from '../../config/swift-keywords.js';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface SundellPattern extends BasePattern {}
-
 const { topicKeywords, qualitySignals } = createSourceConfig(
   { 'protocols': ['associated type'] },
   { 'pattern': 6, 'best practice': 8, 'foundation': 4 }
 );
 
-export class SundellSource extends RssPatternSource<SundellPattern> {
+export class SundellSource extends RssPatternSource<BasePattern> {
   constructor() {
     super({
       feedUrl: 'https://www.swiftbysundell.com/feed.rss',
